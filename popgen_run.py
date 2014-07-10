ae87420b871fb7a433a903b7b5c54c224d2fb682
Lisa Zorn <lisa.zorn@sfcta.org>
2014-07-09 23:12:56 -0700

    Add export results option as it crashes often and we dont want to run the whole thing

--- popgen_run.py
+++ popgen_run.py
@@ -1,7 +1,28 @@
-import sys
+import getopt, sys
 
 from configuration.config_parser import ConfigParser
 from popgen_manager import PopgenManager
+
+
+USAGE = """
+
+ python popgen_run.py [-e] config.xml [0]
+
+ The first argument is the location of the configuration file.
+
+ The second argument is an indicator for running in
+ serial or parallel and is optional; 0 indicates run in serial.
+ Otherwise parallel operation on multiple cores are assumed (if
+ multiple cores are available).
+ 
+ Optional -e is for export-results only (do not run).
+ 
+ e.g. /home/config.xml (linux machine single core)
+     c:/testproject/config.xml (windows machine single core)
+ e.g. /home/config.xml 1(linux machine run in parallel)
+     or c:/testproject/config.xml 1(windows machine run in parallel)
+    
+"""
 
 def run():
     """
@@ -11,29 +32,27 @@ def run():
     Please refer to OpenAMOS documentation on www.simtravel.wikispaces.asu.edu
     for guidance on setting up the configuration file.
     """
-    args = sys.argv[1:]
+    
+    optlist, args = getopt.getopt(sys.argv[1:],'e')
 
     print args
 
     if len(args) < 1 or len(args) > 2:
-        raise ArgumentsError, """The module accepts """\
-            """only one argument which is the location of the configuration """\
-            """file and second argument is an indicator for running in """\
-            """ serial or parallel and is optional - 0 indicates run in serial """\
-            """ otherwise it is always checks for multiple cores and runs in parallel """\
-            """ when multiple cores are available."""\
-            """e.g. /home/config.xml (linux machine single core) """\
-            """or c:/testproject/config.xml (windows machine single core)"""\
-            """e.g. /home/config.xml 1(linux machine run in parallel) """\
-            """or c:/testproject/config.xml 1(windows machine run in parallel)"""\
+        raise ArgumentsError, USAGE
 
     if len(args) == 2:
-	pFlag = args[1]
+        pFlag = args[1]
     else:
     	pFlag = 1
     
+    export_results_only = False
+    for o,a in optlist:
+        if o=='-e':
+            print "Exporting results only"
+            export_results_only = True
+    
     popgenManagerObject = PopgenManager(fileLoc = args[0], parallelFlag=pFlag)
-    popgenManagerObject.run_scenarios()
+    popgenManagerObject.run_scenarios(export_results_only)
 
 
 
